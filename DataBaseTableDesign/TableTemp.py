afa5f53a415457b1bb48c2b7764cda794df39778
HONKONE <honkone@126.com>
2018-03-07 23:24:38 +0800

    2018-03-07

---
+++ DataBaseTableDesign/TableTemp.py
@@ -0,0 +1,35 @@
+{
+    "DataType":"data",#DataType is one of [Domain,IP,Url,DGA,SHA1,SHA256,MD5]
+    "IOCs":[
+        "Update_time":"time",#this is the analysist time from the threat information or the threart information when you got it(if not find time in threat from information)
+        "Source":"str",#where are you got this information
+        "Threat_soce":int,
+        "Categories":["str"],
+        "Familes":["str"],
+        "Organizations":["str"],#APT Organization or Other name of them
+        "Description":"str"
+        "Source_url":["url"]#maybe this information is from blob or analysis page 
+    ],
+    "Relation_urls":[
+        {
+            "url":"str"
+        },
+    ],
+    "Relation_ip":[
+        {
+            "ip":""
+        },
+    ]
+    "Relation_domain":[
+        {
+            "domain":""
+        }
+    ]
+    "Relation_sample":[
+        {
+            "MD5":"xx",
+            "SHA1":"xxx",
+            "SHA256":"xxxx",
+        },
+    ]
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+## Design of Threat Intelligence Database 
+
+---
+
+As a Threat Intelligence Analysis Worker,
+My Graduation design is to build a threat information platform,
+the first thing i throught of ws how to relation the information from diffence threat information source,
+os i should design a table to relation this
+
+
