eeec6d371bce5885b7f565f9807cf1c7681ec523
HONKONE <honkone@126.com>
2018-03-11 00:38:00 +0800

    2018-03-11

--- DataBaseTableDesign/TableTemp.py
+++ DataBaseTableDesign/TableTemp.py
@@ -1,6 +1,8 @@
 {
     "DataType":"data",#DataType is one of [Domain,IP,Url,DGA,SHA1,SHA256,MD5]
+	"Target":[]
     "IOCs":[
+		{
         "Update_time":"time",#this is the analysist time from the threat information or the threart information when you got it(if not find time in threat from information)
         "Source":"str",#where are you got this information
         "Threat_soce":int,
@@ -9,13 +11,14 @@
         "Organizations":["str"],#APT Organization or Other name of them
         "Description":"str"
         "Source_url":["url"]#maybe this information is from blob or analysis page 
-    ],
+		}
+	],
     "Relation_urls":[
         {
             "url":"str"
         },
     ],
-    "Relation_ip":[
+    "Relation_ips":[
         {
             "ip":""
         },
