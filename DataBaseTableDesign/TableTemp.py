ed295546a0082fe7ff555a420fba02b2e9c3705c
HONKONE <honkone@126.com>
2018-05-18 01:15:39 +0800

    update

--- DataBaseTableDesign/TableTemp.py
+++ DataBaseTableDesign/TableTemp.py
@@ -1,38 +1,46 @@
 {
     "DataType":"data",#DataType is one of [Domain,IP,Url,DGA,SHA1,SHA256,MD5]
-	"Target":[]
     "IOCs":[
 		{
-        "Update_time":"time",#this is the analysist time from the threat information or the threart information when you got it(if not find time in threat from information)
-        "Source":"str",#where are you got this information
-        "Threat_soce":int,
-        "Categories":["str"],
-        "Familes":["str"],
-        "Organizations":["str"],#APT Organization or Other name of them
-        "Description":"str"
-        "Source_url":["url"]#maybe this information is from blob or analysis page 
-		}
+                    "Update_time":1521812563,#this is the analysist time from the threat information or the threart information when you got it(if not find time in threat from information)
+                    "Source":"nicai",#where are you got this information
+                    "Threat_soce":90,
+                    "Categories":["str"],
+                    "Familes":["str"],
+                    "Organizations":["str"],#APT Organization or Other name of them
+                    "Description":"str"
+                    "Source_url":["url"]#maybe this information is from blob or analysis page 
+		},
 	],
-    "Relation_urls":[
+    "urls":[
         {
-            "url":"str"
+            "scan_time":12452342,
+            "source":"twitter",
+            "url":"http://login.libero.it.imapssl.eu/aggiornamento-email.php"
         },
     ],
-    "Relation_ips":[
+    "ips":[
         {
-            "ip":""
+            
+            "ip":"132.4.5.6",
+            "update_time":23523423,
+            "source":"otx"
+            
         },
-    ]
-    "Relation_domain":[
+    ],
+    "domains":[
         {
-            "domain":""
-        }
-    ]
-    "Relation_sample":[
-        {
-            "MD5":"xx",
-            "SHA1":"xxx",
-            "SHA256":"xxxx",
+            "source":"MalwareConfig",
+            "domain":"login.libero.it.imapssl.eu",
+            "update_time":1521830582£¬
+            "threat_score":90
         },
-    ]
+    ],
+    "samples":[
+        {
+            "source":"Vt",
+            "sha256":"xxxxxxxxxxxxxxxxxxxxxx",
+            "update_time":1521823378,
+        },
+    ],
 }
